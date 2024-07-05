d7cb710163595181f6d398628b86846596d40635
firdavsxon@gmail.com <firdavsxon@gmail.com>
2024-07-05 11:00:15 +0500

    9-vazifa 5-oy

---
+++ js/aps.js
@@ -0,0 +1,44 @@
+
+
+const countries = document.getElementById('countries');
+const loading = document.getElementById('loading');
+const card = document.querySelectorAll('.card');
+const showBtn = document.querySelectorAll('#showBtn')
+
+function creatCard(counter){
+    return `
+       <div class="card">
+                <img src="${counter.flag}" alt="">
+                <div class="text">
+                    <h3>Name: ${counter.country}</h3>
+                    <h4>Code: ${counter.code}</h4>
+                    <h4>ID: ${counter.id}</h4>
+                </div>
+                <a id='showBtn' class='show' href="./pages/about.html">Show</a>
+        </div>
+    `
+}
+document.addEventListener('DOMContentLoaded', function(){
+    fetch("https://cars-pagination.onrender.com/all-countries")
+        .then(function(res){
+            if(res.status < 300){
+                return res.json()
+            }
+        })
+        .then(function(data){
+            // countries.style.display = 'block';
+            data.length > 0 && data.forEach(counter => {
+                const card = creatCard(counter);
+                countries.innerHTML += card;
+            })
+        })
+        .catch(err => {
+            countries.style.display = 'block';
+            console.log(err)
+        })
+        .finally(function(){
+            loading.style.display = 'none'
+        })
+})
+
+
